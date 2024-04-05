7a667fa05d5051db752c6a4330c2c610531f6914
horvathnauzika <naihorvath@gmail.com>
2024-04-05 09:35:02 +0200

    listamuveletek

---
+++ adat.js
@@ -0,0 +1,42 @@
+export const emberekLISTA=[
+    {
+        nev: "Jenő",
+        kor: 23,
+        nem: true
+    },
+    {
+        nev: "Juli",
+        kor: 33,
+        nem: false
+    },
+    {
+        nev: "Géza",
+        kor: 13,
+        nem: true
+    },
+    {
+        nev: "Béla",
+        kor: 18,
+        nem: true
+    },
+    {
+        nev: "Mari",
+        kor: 27,
+        nem: false
+    },
+    {
+        nev: "Ubul",
+        kor: 26,
+        nem: true
+    },
+    {
+        nev: "Manó",
+        kor: 21,
+        nem: true
+    },
+    {
+        nev: "Fanni",
+        kor: 36,
+        nem: false
+    }
+]
